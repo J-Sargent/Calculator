06ac7929b8c38f6dc79f6318e7d1e5a75d27302c
J-Sargent <blythetruth@gmail.com>
2019-04-16 17:12:06 -0500

    sum works :D

--- secondCalculatorScript.js
+++ secondCalculatorScript.js
@@ -6,11 +6,11 @@ function getNumbers(){
 	var secondInput = document.getElementById('secondNumber');
 	var secondNumber = secondInput.value;
 	var numberCheck = [firstNumber,secondNumber];
-	numberCheck.forEach(isANumber);
-	var numberCheck = [1,2];
-	numberCheck.forEach(makeANumber);
-	var sum = firstNumber + secondNumber;
-	console.log(sum);
+	//numberCheck.forEach(isANumber);
+	var newNumbers = numberCheck.map(makeANumber);
+	//newNumbers.forEach(isANumber);
+	var sumOne = newNumbers[0] + newNumbers[1];
+	console.log(sumOne);
 }
 
 
@@ -21,17 +21,18 @@ var fruits = ["apple","bannana"];
 fruits.forEach(function(item,index,array){console.log(item,index);});*/
 
 
+/*checking to see if it is a number
 function isANumber(item,index){
 	if(typeof item =="number"){
 		console.log(index + " is a number");
 	} else {
 		console.log(index + " is not a number");
 	}
-}
+} */
 
 function makeANumber(item,index){
-		Number(item);
-		isANumber(item,index);
+		var parsed = parseFloat(item);
+		return parsed;
 }
 
 
