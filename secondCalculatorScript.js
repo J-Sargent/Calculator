3e63f642bda1259ae17d168084b8d262a1d599a6
J-Sargent <blythetruth@gmail.com>
2019-04-16 16:55:52 -0500

    function makeANumber works on console log, but won't sum

--- secondCalculatorScript.js
+++ secondCalculatorScript.js
@@ -1,11 +1,39 @@
-function logThis(){
+function getNumbers(){
 	console.log('function called');
-	event.preventDefault();
-	console.log(document.getElementById('firstNumber').value);
-	var firstNumber = (document.getElementById('firstNumber').value);
 
-		
-	var secondNumber = (document.getElementById('secondNumber').value);
+	var firstInput = document.getElementById('firstNumber');
+	var firstNumber = firstInput.value;
+	var secondInput = document.getElementById('secondNumber');
+	var secondNumber = secondInput.value;
+	var numberCheck = [firstNumber,secondNumber];
+	numberCheck.forEach(isANumber);
+	var numberCheck = [1,2];
+	numberCheck.forEach(makeANumber);
 	var sum = firstNumber + secondNumber;
 	console.log(sum);
 }
+
+
+
+/*
+array demo
+var fruits = ["apple","bannana"];
+fruits.forEach(function(item,index,array){console.log(item,index);});*/
+
+
+function isANumber(item,index){
+	if(typeof item =="number"){
+		console.log(index + " is a number");
+	} else {
+		console.log(index + " is not a number");
+	}
+}
+
+function makeANumber(item,index){
+		Number(item);
+		isANumber(item,index);
+}
+
+
+
+//var a - parseFloat("10");
