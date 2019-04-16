b6c56d3514e501cfc46d458d71f80f30ebc5274f
J-Sargent <blythetruth@gmail.com>
2019-04-16 11:26:59 -0500

    variables coming through as strings

--- secondCalculatorScript.js
+++ secondCalculatorScript.js
@@ -1,4 +1,11 @@
 function logThis(){
-	console.log("function works");
+	console.log('function called');
+	event.preventDefault();
 	console.log(document.getElementById('firstNumber').value);
+	var firstNumber = (document.getElementById('firstNumber').value);
+
+		
+	var secondNumber = (document.getElementById('secondNumber').value);
+	var sum = firstNumber + secondNumber;
+	console.log(sum);
 }
