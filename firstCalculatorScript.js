7e2fca8cda3a946ba71a04374e5b9acbdb0884df
J-Sargent <blythetruth@gmail.com>
2019-04-16 10:42:08 -0500

    end first try, begin second

--- firstCalculatorScript.js
+++ firstCalculatorScript.js
@@ -4,6 +4,24 @@ console.log("hi");
 //var b = 5;
 //alert("Sum is "+ (a + b));
 
-var a = prompt();
-var b = 5;
-alert("Sum is "+ (a + b));
+//var a = prompt();
+//var b = 5;
+//alert("Sum is "+ (a + b));
+
+//function test()
+//{
+//var firstName = document.getElementByID(firstName).value;
+//console.log(firstName);
+//}
+
+function submitForm(event){
+	console.log('function called');
+	event.preventDefault();
+	console.log('Prevented reload');
+	firstNameInput();
+}
+
+function firstNameInput() {
+	var firstName = document.getElementById("firstName").value;
+	console.log(firstName);
+}
