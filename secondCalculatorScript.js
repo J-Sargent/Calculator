51ed7a088d82dacc66bd43bf5e44ef1a9e7928fd
J-Sargent <blythetruth@gmail.com>
2019-04-21 18:43:54 -0500

    functional, needs several things including reset.

--- secondCalculatorScript.js
+++ secondCalculatorScript.js
@@ -1,105 +1,55 @@
-
-
-
-/*
-function getNumbers(){
-	console.log('function called');
-
-	var firstInput = document.getElementById('firstBoxNumber');
-	var firstNumber = firstInput.value;
-	var secondInput = document.getElementById('secondBoxNumber');
-	var secondNumber = secondInput.value;
-	var numberCheck = [firstNumber,secondNumber];
-	//numberCheck.forEach(isANumber);
-	var newNumbers = numberCheck.map(makeANumber);
-	//newNumbers.forEach(isANumber);
-	var sumOne = newNumbers[0] + newNumbers[1];
-	console.log(sumOne);
-}
-*/
-
-
 window.preNumbers = [];
 window.savedNumbers =[];
-window.clickNumber = 0;
 window.currentFullNumber = null;
 window.answer = null;
-window.operator = [];
+window.textOperator;
 
 function reset(){
-	preNumbers =[];
+	preNumbers = [];
 	//clear out displayBox
 }
 
 function getButtons(){
 	var a = event.target.value;
-	//console.log(a);
 	preNumbers.push(a);
-	clickNumber++;
-	//console.log("clicks: " + clickNumber);
 	currentFullNumber = parseInt(preNumbers.join(''));
 	document.getElementById("displayBox").innerHTML = currentFullNumber;
 }
 
 function nextOperator(){
-	console.log("CurrentFullNumber is " + currentFullNumber);
 	savedNumbers.push(currentFullNumber);
-	console.log("0 position of savedNumbers is " + savedNumbers[0]);
-	console.log("1 position of savedNumbers is " + savedNumbers[1]);
-	var textOperator = event.target.value;
-	console.log("The value of this button is " + textOperator);
-
-	switch(textOperator) {
-		case textOperator = "plus":
-		console.log("plus");
-		answer = savedNumbers[0] + savedNumbers[1];
-		break;
-		case textOperator = "minus":
-		console.log("minus");
-		answer = savedNumbers[0] - savedNumbers[1];
-		break;
-		default:
-		console.log("It did not work");
-	}
-		reset();
+	textOperator = event.target.value;
+	reset();
 }
 
 function displayAnswer(){
-	console.log("okkkkkkk");
+	savedNumbers.push(currentFullNumber);
 
+	switch(textOperator) {
+		case "plus":
+		answer = savedNumbers[0] + savedNumbers[1];
+		parseInt(answer);
+		break;
 
-	document.getElementById("displayAnswer").innerHTML = "The final answer is " + answer;
+		case "minus":
+		answer = savedNumbers[0] - savedNumbers[1];
+		parseInt(answer);
+		break;
+
+		case "multiply":
+		answer = savedNumbers[0] * savedNumbers[1];
+		parseInt(answer);
+		break;
+
+		case "divide":
+		answer = savedNumbers[0] / savedNumbers[1];
+		parseInt(answer);
+		break;
+
+		default:
+		console.log("It did not work");
+	}
+savedNumbers = [];
+savedNumbers.push(answer);
+	document.getElementById("displayAnswer").innerHTML = "The final answer is: " + answer;
 }
-
-
-/*
-}
-*/
-
-
-
-//var fullNumber = parseInt(preNumbers.join(''));
-
-															/*
-											array demo
-											var fruits = ["apple","bannana"];
-											fruits.forEach(function(item,index,array){console.log(item,index);});*/
-
-
-											/*checking to see if it is a number
-											function isANumber(item,index){
-												if(typeof item =="number"){
-													console.log(index + " is a number");
-												} else {
-													console.log(index + " is not a number");
-												}
-											} */
-
-function makeANumber(item,index){
-		var parsed = parseFloat(item);
-		return parsed;
-}
-
-
-
-//var a - parseFloat("10");
