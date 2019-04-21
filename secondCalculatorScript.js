f686b0458254649f709070e40b33b58f12f57418
J-Sargent <blythetruth@gmail.com>
2019-04-21 12:56:46 -0500

    beginning of branch newButtons

--- secondCalculatorScript.js
+++ secondCalculatorScript.js
@@ -4,9 +4,9 @@ ALL OF THE NUMBERS FROM THE PAGE AT ONCE */
 function getNumbers(){
 	console.log('function called');
 
-	var firstInput = document.getElementById('firstNumber');
+	var firstInput = document.getElementById('firstBoxNumber');
 	var firstNumber = firstInput.value;
-	var secondInput = document.getElementById('secondNumber');
+	var secondInput = document.getElementById('secondBoxNumber');
 	var secondNumber = secondInput.value;
 	var numberCheck = [firstNumber,secondNumber];
 	//numberCheck.forEach(isANumber);
@@ -16,23 +16,29 @@ function getNumbers(){
 	console.log(sumOne);
 }
 
-//one linke can replace getNumbers lns 4-8
+function getButtons(){
+	console.log("getbuttons was called");
+	var numberButtons = [];
+	numberButtons = document.getElementsByClassName('numberButtons');
+	console.log(numberButtons.item(0) + ", " + numberButtons.item(1));
+}
+//one lineI  can replace getNumbers lns 4-8
 
 
-/*
-array demo
-var fruits = ["apple","bannana"];
-fruits.forEach(function(item,index,array){console.log(item,index);});*/
+															/*
+											array demo
+											var fruits = ["apple","bannana"];
+											fruits.forEach(function(item,index,array){console.log(item,index);});*/
 
 
-/*checking to see if it is a number
-function isANumber(item,index){
-	if(typeof item =="number"){
-		console.log(index + " is a number");
-	} else {
-		console.log(index + " is not a number");
-	}
-} */
+											/*checking to see if it is a number
+											function isANumber(item,index){
+												if(typeof item =="number"){
+													console.log(index + " is a number");
+												} else {
+													console.log(index + " is not a number");
+												}
+											} */
 
 function makeANumber(item,index){
 		var parsed = parseFloat(item);
