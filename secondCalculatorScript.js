11456a416e0b6dd8be52e0d24c79afab909aa4a5
J-Sargent <blythetruth@gmail.com>
2019-04-23 17:05:34 -0500

    symbol keys working, now enter for =s

--- secondCalculatorScript.js
+++ secondCalculatorScript.js
@@ -4,13 +4,22 @@ window.currentFullNumber = null;
 window.answer = null;
 window.textOperator;
 window.keyThatWasPressed = null;
+window.equationString = "The equation is "; //turn this into an array next arrayName.join(" ");
+window.symbols = ['/','*','-','+'];
 
 //typing
+
 function keyPress(e){
 	var key = e.key;
-	makeNumber(key);
+if (!isNaN(key)) {makeNumber(key);
+}
+else if(symbols.indexOf(key) >= 0) {
+	console.log("a symbol" + key)
+	keyOperator(key);
+	}
 }
 addEventListener('keydown', keyPress);
+
 
 //reset
 function reset(){
@@ -26,17 +35,29 @@ function makeNumber(a){
 	preNumbers += a;
 	currentFullNumber = parseInt(preNumbers);
 	var displayBox = document.getElementById("displayBox");
-	displayBox.textContent = currentFullNumber + " "; //fix display at some point
+	displayBox.textContent = currentFullNumber + " ";
+	updateDisplay(a); //fix display at some point
 }
 
 //capture operators
 function nextOperator(){
-	textOperator = "huh";
+	//console.log("nextOperator called with key " + key);
 	if (currentFullNumber) {	//this does the same thing as currentFullNumber !== null
 		savedNumbers.push(currentFullNumber);
 	}
 	textOperator = event.target.value;
-	displayBox.textContent += textOperator + " ";
+	updateDisplay(textOperator);
+	preNumbers = [];
+	currentFullNumber = null;
+}
+
+function keyOperator(key){
+	if (currentFullNumber) {	//this does the same thing as currentFullNumber !== null
+		savedNumbers.push(currentFullNumber);
+	}
+	textOperator = key;
+  console.log("in keyOperator the textOperator is " + textOperator);
+	updateDisplay(textOperator);
 	preNumbers = [];
 	currentFullNumber = null;
 }
@@ -74,4 +95,19 @@ function displayAnswer(){
 	savedNumbers.push(answer);
 	preNumbers = 0;
 	currentFullNumber = null;
+}
+
+//display update
+
+function updateDisplay(b){
+	equationString += b;
+	var equationBox = document.getElementById("equationBox");
+	equationBox.textContent = equationString;
+	//document.getElementById("equationBox").innerHTML = equationString;
+	//console.log("updateDisplay called");
+	//var count;
+	//console.log("count before iteration is " +count);
+	//count = count++;
+	//console.log(count);
+	//document.getElementById("equationBox").innerHTML = currentFullNumber + textOperator;
 }
